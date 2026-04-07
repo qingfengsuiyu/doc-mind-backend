86085a820b4f588842ec7f4bed59516c06421876
qingfengsuiyu <xiaoyihao2016@gmail.com>
2026-04-07 20:41:59 +0800

    feat: 换用DashScope embedding API，添加文档删除功能

--- database.py
+++ database.py
@@ -1,10 +1,13 @@
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.embeddings import DashScopeEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
+import os
+
 
 # 加载 embedding 模型（第一次会自动下载，需要等一会）
-embeddings = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
+embeddings = DashScopeEmbeddings(
+    model="text-embedding-v3",
+    dashscope_api_key=os.getenv("sk-155ccd2b87ed4d049666f3c98cb6b60d")
 )
 
 splitter = RecursiveCharacterTextSplitter(
